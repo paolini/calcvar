2c085b3adc1951c92070ea13033da93b913165f6
Emanuele Paolini <emanuele.paolini@gmail.com>
2023-02-02 12:51:25 +0100

    primo rendering * configurato katex * configurato bootstrap

---
+++ index.js
@@ -0,0 +1,16 @@
+function myRender() {
+    console.log(`myRender() called`)
+    // • renderMathInElement() is a function from KaTeX
+    renderMathInElement(document.body, {
+        // customised options
+        // • auto-render specific keys, e.g.:
+        delimiters: [
+            {left: '$$', right: '$$', display: true},
+            {left: '$', right: '$', display: false},
+            {left: '\\(', right: '\\)', display: false},
+            {left: '\\[', right: '\\]', display: true}
+        ],
+        // • rendering keys, e.g.:
+        throwOnError : false
+      });
+}
